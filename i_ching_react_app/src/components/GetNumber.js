cb62ba74d2d2df4a8169200885c2e13eba7797db
erikksuzuki <erikksuzuki@gmail.com>
2020-05-13 13:04:12 +0800

    switchcase for hexagram numbers

--- i_ching_react_app/src/components/GetNumber.js
+++ i_ching_react_app/src/components/GetNumber.js
@@ -1,201 +1,5 @@
 function GetNumber(props) {
   var hexastringprop = props.string;
-  function getNumber(hexvalue) {
-    var hexanumber;
-    if (hexvalue == "VVVVVV") {
-      hexanumber = 1;
-    }
-    if (hexvalue == "PPPPPP") {
-      hexanumber = 2;
-    }
-    if (hexvalue == "VPPPVP") {
-      hexanumber = 3;
-    }
-    if (hexvalue == "PVPPPV") {
-      hexanumber = 4;
-    }
-    if (hexvalue == "VVVPVP") {
-      hexanumber = 5;
-    }
-    if (hexvalue == "PVPVVV") {
-      hexanumber = 6;
-    }
-    if (hexvalue == "PVPPPP") {
-      hexanumber = 7;
-    }
-    if (hexvalue == "PPPPVP") {
-      hexanumber = 8;
-    }
-    if (hexvalue == "VVVPVV") {
-      hexanumber = 9;
-    }
-    if (hexvalue == "VVPVVV") {
-      hexanumber = 10;
-    }
-    if (hexvalue == "VVVPPP") {
-      hexanumber = 11;
-    }
-    if (hexvalue == "PPPVVV") {
-      hexanumber = 12;
-    }
-    if (hexvalue == "VPVVVV") {
-      hexanumber = 13;
-    }
-    if (hexvalue == "VVVVPV") {
-      hexanumber = 14;
-    }
-    if (hexvalue == "PPVPPP") {
-      hexanumber = 15;
-    }
-    if (hexvalue == "PPPVPP") {
-      hexanumber = 16;
-    }
-    if (hexvalue == "VPPVVP") {
-      hexanumber = 17;
-    }
-    if (hexvalue == "PVVPPV") {
-      hexanumber = 18;
-    }
-    if (hexvalue == "VVPPPP") {
-      hexanumber = 19;
-    }
-    if (hexvalue == "PPPPVV") {
-      hexanumber = 20;
-    }
-    if (hexvalue == "VPPVPV") {
-      hexanumber = 21;
-    }
-    if (hexvalue == "VPVPPV") {
-      hexanumber = 22;
-    }
-    if (hexvalue == "PPPPPV") {
-      hexanumber = 23;
-    }
-    if (hexvalue == "VPPPPP") {
-      hexanumber = 24;
-    }
-    if (hexvalue == "VPPVVV") {
-      hexanumber = 25;
-    }
-    if (hexvalue == "VVVPPV") {
-      hexanumber = 26;
-    }
-    if (hexvalue == "VPPPPV") {
-      hexanumber = 27;
-    }
-    if (hexvalue == "PVVVVP") {
-      hexanumber = 28;
-    }
-    if (hexvalue == "PVPPVP") {
-      hexanumber = 29;
-    }
-    if (hexvalue == "VPVVPV") {
-      hexanumber = 30;
-    }
-    if (hexvalue == "PPVVVP") {
-      hexanumber = 31;
-    }
-    if (hexvalue == "PVVVPP") {
-      hexanumber = 32;
-    }
-    if (hexvalue == "PPVVVV") {
-      hexanumber = 33;
-    }
-    if (hexvalue == "VVVVPP") {
-      hexanumber = 34;
-    }
-    if (hexvalue == "PPPVPV") {
-      hexanumber = 35;
-    }
-    if (hexvalue == "VPVPPP") {
-      hexanumber = 36;
-    }
-    if (hexvalue == "VPVPVV") {
-      hexanumber = 37;
-    }
-    if (hexvalue == "VVPVPV") {
-      hexanumber = 38;
-    }
-    if (hexvalue == "PPVPVP") {
-      hexanumber = 39;
-    }
-    if (hexvalue == "PVPVPP") {
-      hexanumber = 40;
-    }
-    if (hexvalue == "VVPPPV") {
-      hexanumber = 41;
-    }
-    if (hexvalue == "VPPPVV") {
-      hexanumber = 42;
-    }
-    if (hexvalue == "VVVVVP") {
-      hexanumber = 43;
-    }
-    if (hexvalue == "PVVVVV") {
-      hexanumber = 44;
-    }
-    if (hexvalue == "PPPVVP") {
-      hexanumber = 45;
-    }
-    if (hexvalue == "PVVPPP") {
-      hexanumber = 46;
-    }
-    if (hexvalue == "PVPVVP") {
-      hexanumber = 47;
-    }
-    if (hexvalue == "PVVPVP") {
-      hexanumber = 48;
-    }
-    if (hexvalue == "VPVVVP") {
-      hexanumber = 49;
-    }
-    if (hexvalue == "PVVVPV") {
-      hexanumber = 50;
-    }
-    if (hexvalue == "VPPVPP") {
-      hexanumber = 51;
-    }
-    if (hexvalue == "PPVPPV") {
-      hexanumber = 52;
-    }
-    if (hexvalue == "PPVPVV") {
-      hexanumber = 53;
-    }
-    if (hexvalue == "VVPVPP") {
-      hexanumber = 54;
-    }
-    if (hexvalue == "VPVVPP") {
-      hexanumber = 55;
-    }
-    if (hexvalue == "PPVVPV") {
-      hexanumber = 56;
-    }
-    if (hexvalue == "PVVPVV") {
-      hexanumber = 57;
-    }
-    if (hexvalue == "VVPVVP") {
-      hexanumber = 58;
-    }
-    if (hexvalue == "PVPPVV") {
-      hexanumber = 59;
-    }
-    if (hexvalue == "VVPPVP") {
-      hexanumber = 60;
-    }
-    if (hexvalue == "VVPPVV") {
-      hexanumber = 61;
-    }
-    if (hexvalue == "PPVVPP") {
-      hexanumber = 62;
-    }
-    if (hexvalue == "VPVPVP") {
-      hexanumber = 63;
-    }
-    if (hexvalue == "PVPVPV") {
-      hexanumber = 64;
-    }
-    return hexanumber;
-  }
 
   function hexagramnumber() {
     var gramvalue = hexastringprop;
@@ -205,6 +9,74 @@ function GetNumber(props) {
     }
     return getNumber(gramvalue);
   }
+
+  const getNumber = (hexvalue) =>
+    ({
+      VVVVVV: 1,
+      PPPPPP: 2,
+      VPPPVP: 3,
+      PVPPPV: 4,
+      VVVPVP: 5,
+      PVPVVV: 6,
+      PVPPPP: 7,
+      PPPPVP: 8,
+      VVVPVV: 9,
+      VVPVVV: 10,
+      VVVPPP: 11,
+      PPPVVV: 12,
+      VPVVVV: 13,
+      VVVVPV: 14,
+      PPVPPP: 15,
+      PPPVPP: 16,
+      VPPVVP: 17,
+      PVVPPV: 18,
+      VVPPPP: 19,
+      PPPPVV: 20,
+      VPPVPV: 21,
+      VPVPPV: 22,
+      PPPPPV: 23,
+      VPPPPP: 24,
+      VPPVVV: 25,
+      VVVPPV: 26,
+      VPPPPV: 27,
+      PVVVVP: 28,
+      PVPPVP: 29,
+      VPVVPV: 30,
+      PPVVVP: 31,
+      PVVVPP: 32,
+      PPVVVV: 33,
+      VVVVPP: 34,
+      PPPVPV: 35,
+      VPVPPP: 36,
+      VPVPVV: 37,
+      VVPVPV: 38,
+      PPVPVP: 39,
+      PVPVPP: 40,
+      VVPPPV: 41,
+      VPPPVV: 42,
+      VVVVVP: 43,
+      PVVVVV: 44,
+      PPPVVP: 45,
+      PVVPPP: 46,
+      PVPVVP: 47,
+      PVVPVP: 48,
+      VPVVVP: 49,
+      PVVVPV: 50,
+      VPPVPP: 51,
+      PPVPPV: 52,
+      PPVPVV: 53,
+      VVPVPP: 54,
+      VPVVPP: 55,
+      PPVVPV: 56,
+      PVVPVV: 57,
+      VVPVVP: 58,
+      PVPPVV: 59,
+      VVPPVP: 60,
+      VVPPVV: 61,
+      PPVVPP: 62,
+      VPVPVP: 63,
+      PVPVPV: 64,
+    }[hexvalue]);
 
   return hexagramnumber();
 }
